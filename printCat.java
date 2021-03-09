dee5d40601e8d5c5d9beb8a357a1e5485d9acdef
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-09 19:04:28 +0900

    백준 10171번 고양이 출력 문제

---
+++ printCat.java
@@ -0,0 +1,10 @@
+class printCat 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|"); //java에서 역슬래시 출력은 \\두번으로 사용한다. 파이썬에서는 '\' 또는 "\" 이런 식으로 따옴표로 묶어 표현했던 것과 비교가 된다.
+	}
+}
