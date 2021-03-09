5be7aeef4640f9cbf1ecc5775ae93933c2d68b2c
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-09 19:10:32 +0900

    백준 10172번 개 출력 문제

---
+++ printDog.java
@@ -0,0 +1,16 @@
+class  printDog
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\"); //java에서 쌍따옴표 출력은 \"로 출력한다.
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\__|");
+	}
+}
+
+
+
+
+
