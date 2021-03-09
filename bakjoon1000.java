845ff80241064233ae1bba0e93682e6609c9c5d5
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-09 19:42:41 +0900

    백준 1000번 A+B 입력과 출력 문제

---
+++ bakjoon1000.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; //class scanner는 사용자로부터 입력받은 후, 이후에 변수값으로 사용하는 class이다
+
+
+class  bakjoon1000
+{
+	public static void main(String[] args) 
+	{
+		Scanner plus=new Scanner(System.in); //사용자로부터 받은 변수들의 값을 하나의 객체로 만들어 바이트 단위로 읽는다.
+		System.out.println("A와 B를 입력하세요");
+		int A=plus.nextInt(); //nextInt()는 scanner class 속의 method로 입력된 것중 공백 이전까지의 문자열을 정수형으로 받음
+		int B=plus.nextInt();
+		int result=A+B;
+		System.out.println("A+B= "+ result);
+	}
+}
