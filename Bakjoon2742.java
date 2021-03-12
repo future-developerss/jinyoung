82542937969185abff86230066cbc0bc86129d9a
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-12 18:03:09 +0900

    백준 2742 for문-n부터 거꾸로 출력하는 문제 풀이

---
+++ Bakjoon2742.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class  Bakjoon2742
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner=new Scanner(System.in);
+		int count=scanner.nextInt();
+		for (int i=count; i>0; i--)
+		{
+			System.out.println(i);
+		}
+	}
+}
