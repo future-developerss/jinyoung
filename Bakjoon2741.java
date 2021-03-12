d8c781f5b93fa6cc40f336590fc3128fd7a3fb57
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-12 17:56:58 +0900

    백준 2741 for문-n까지 차례대로 출력하는 문제 풀이

---
+++ Bakjoon2741.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class  Bakjoon2741
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner=new Scanner(System.in);
+		int count=scanner.nextInt();
+		for (int i=1; i<count+1; i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
