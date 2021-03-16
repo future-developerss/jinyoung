5029e8b5893e1dcc4e99fdf787fe7e72d2daef2f
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-16 20:15:41 +0900

    for 반복문-A+B 8차 문제 풀이

---
+++ Bakjoon11022.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class  Bakjoon11022
+{
+	public static void main(String[] args) 
+	{
+		Scanner scanner=new Scanner(System.in);
+		int count=scanner.nextInt();
+			for (int i=1;i<count+1 ;i++ )
+		{
+			int a= scanner.nextInt();
+			int b= scanner.nextInt();
+			int result= a+b;
+			System.out.println("Case #"+i+": "+a+" + "+b+" = "+result);
+		}
+	}
+}
