b35012e4020c6b9caf8ac38421fecec4ef2e59fb
zhenying2 <khappy9888@sookmyung.ac.kr>
2021-03-24 21:14:05 +0900

    문자열-아스키코드 출력하기

---
+++ Bakjoon11654.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+ 
+public class Bakjoon11654 {
+	public static void main(String[] args) {
+ 
+		Scanner scanner = new Scanner(System.in);
+ 
+		int ch = scanner.next().charAt(0);
+        
+		System.out.print(ch);
+	}
+}
